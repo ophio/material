0ca2bd29d1cc49ea77418f2809b0eb9598dd4ffb
Gaurav <vashisthg@gmail.com>
2014-08-01 16:54:13 +0530

    material button

---
+++ app/src/classic/java/com/vashisthg/androidl/widget/AbstractAnimatedPreviewButton.java
@@ -0,0 +1,21 @@
+package com.vashisthg.androidl.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by gvv-fueled on 01/08/14.
+ */
+public abstract class AbstractAnimatedPreviewButton extends CheckableFrameLayout  {
+    public AbstractAnimatedPreviewButton(Context context) {
+        super(context);
+    }
+
+    public AbstractAnimatedPreviewButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public AbstractAnimatedPreviewButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
