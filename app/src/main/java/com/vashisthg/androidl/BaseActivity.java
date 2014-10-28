dc46b0a4abe5b15d91e6bad9bee13ee7a925debe
Gaurav Vashisth <vashisthg@gmail.com>
2014-10-28 16:53:58 +0530

    added toolbar

--- app/src/main/java/com/vashisthg/androidl/BaseActivity.java
+++ app/src/main/java/com/vashisthg/androidl/BaseActivity.java
@@ -1,21 +1,82 @@
 package com.vashisthg.androidl;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.TypeEvaluator;
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
+
+import com.vashisthg.androidl.utils.LUtils;
+import com.vashisthg.androidl.utils.PrefUtils;
+import com.vashisthg.androidl.widget.ScrimInsetsScrollView;
+
+import java.util.ArrayList;
 
 /**
  * Created by gvv-fueled on 28/10/14.
  */
 public abstract class BaseActivity extends ActionBarActivity {
 
+    private static final String LOGTAG = BaseActivity.class.getSimpleName();
+    protected static final int NAVDRAWER_ITEM_INVALID = -1;
+    protected static final int NAVDRAWER_ITEM_NOT_INVALID = 1;
+
+    // Durations for certain animations we use:
+    private static final int HEADER_HIDE_ANIM_DURATION = 300;
+
     protected Toolbar mActionBarToolbar;
+    protected DrawerLayout mDrawerLayout;
+
+    // Helper methods for L APIs
+    private LUtils mLUtils;
+
+    // A Runnable that we should execute when the navigation drawer finishes its closing animation
+    private Runnable mDeferredOnDrawerClosedRunnable;
+
+    // variables that control the Action Bar auto hide behavior (aka "quick recall")
+    private boolean mActionBarAutoHideEnabled = false;
+    private int mActionBarAutoHideSensivity = 0;
+    private int mActionBarAutoHideMinY = 0;
+    private int mActionBarAutoHideSignal = 0;
+    private boolean mActionBarShown = true;
+
+    private int mProgressBarTopWhenActionBarShown;
+
+    private int mThemedStatusBarColor;
+    private int mNormalStatusBarColor;
+
+    private ObjectAnimator mStatusBarColorAnimator;
+
+    // When set, these components will be shown/hidden in sync with the action bar
+    // to implement the "quick recall" effect (the Action Bar and the header views disappear
+    // when you scroll down a list, and reappear quickly when you scroll up).
+    private ArrayList<View> mHideableHeaderViews = new ArrayList<View>();
+
+    private static final TypeEvaluator ARGB_EVALUATOR = new ArgbEvaluator();
+
+    // SwipeRefreshLayout allows the user to swipe the screen down to trigger a manual refresh
+    private SwipeRefreshLayout mSwipeRefreshLayout;
 
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+
+        mLUtils = LUtils.getInstance(this);
+        mThemedStatusBarColor = getResources().getColor(R.color.theme_primary_dark);
+        mNormalStatusBarColor = mThemedStatusBarColor;
 
     }
 
@@ -23,6 +84,12 @@ public abstract class BaseActivity extends ActionBarActivity {
     public void setContentView(int layoutResID) {
         super.setContentView(layoutResID);
         getActionBarToolbar();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        setupNavDrawer();
     }
 
     protected Toolbar getActionBarToolbar() {
@@ -33,5 +100,206 @@ public abstract class BaseActivity extends ActionBarActivity {
             }
         }
         return mActionBarToolbar;
+    }
+
+
+    /**
+     * Returns the navigation drawer item that corresponds to this Activity. Subclasses
+     * of BaseActivity override this to indicate what nav drawer item corresponds to them
+     * Return NAVDRAWER_ITEM_INVALID to mean that this Activity should not have a Nav Drawer.
+     */
+    protected int getSelfNavDrawerItem() {
+        return NAVDRAWER_ITEM_INVALID;
+    }
+
+    /**
+     * Sets up the navigation drawer as appropriate. Note that the nav drawer will be
+     * different depending on whether the attendee indicated that they are attending the
+     * event on-site vs. attending remotely.
+     */
+    private void setupNavDrawer() {
+        // What nav drawer item should be selected?
+        int selfItem = getSelfNavDrawerItem();
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (mDrawerLayout == null) {
+            return;
+        }
+        mDrawerLayout.setStatusBarBackgroundColor(
+                getResources().getColor(R.color.theme_primary_dark));
+        ScrimInsetsScrollView navDrawer = (ScrimInsetsScrollView)
+                mDrawerLayout.findViewById(R.id.navdrawer);
+        if (selfItem == NAVDRAWER_ITEM_INVALID) {
+            // do not show a nav drawer
+            if (navDrawer != null) {
+                ((ViewGroup) navDrawer.getParent()).removeView(navDrawer);
+            }
+            mDrawerLayout = null;
+            return;
+        }
+
+        if (navDrawer != null) {
+            navDrawer.setOnInsetsCallback(new ScrimInsetsScrollView.OnInsetsCallback() {
+                @Override
+                public void onInsetsChanged(Rect insets) {
+                    Log.d(LOGTAG, "insets changed");
+                }
+            });
+        }
+
+        if (mActionBarToolbar != null) {
+            mActionBarToolbar.setNavigationIcon(R.drawable.ic_drawer);
+            mActionBarToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mDrawerLayout.openDrawer(Gravity.START);
+                }
+            });
+        }
+
+        mDrawerLayout.setDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // run deferred action, if we have one
+                if (mDeferredOnDrawerClosedRunnable != null) {
+                    mDeferredOnDrawerClosedRunnable.run();
+                    mDeferredOnDrawerClosedRunnable = null;
+                }
+
+                onNavDrawerStateChanged(false, false);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                onNavDrawerStateChanged(true, false);
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+                onNavDrawerStateChanged(isNavDrawerOpen(), newState != DrawerLayout.STATE_IDLE);
+            }
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                onNavDrawerSlide(slideOffset);
+            }
+        });
+
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, Gravity.START);
+
+        // populate the nav drawer with the correct items
+        populateNavDrawer();
+
+        // When the user runs the app for the first time, we want to land them with the
+        // navigation drawer open. But just the first time.
+        if (!PrefUtils.isWelcomeDone(this)) {
+            // first run of the app starts with the nav drawer open
+            PrefUtils.markWelcomeDone(this);
+            mDrawerLayout.openDrawer(Gravity.START);
+        }
+    }
+
+    // Subclasses can override this for custom behavior
+    protected void onNavDrawerStateChanged(boolean isOpen, boolean isAnimating) {
+        if (mActionBarAutoHideEnabled && isOpen) {
+            autoShowOrHideActionBar(true);
+        }
+    }
+
+    protected void onNavDrawerSlide(float offset) {}
+
+    protected boolean isNavDrawerOpen() {
+        return mDrawerLayout != null && mDrawerLayout.isDrawerOpen(Gravity.START);
+    }
+
+    protected void closeNavDrawer() {
+        if (mDrawerLayout != null) {
+            mDrawerLayout.closeDrawer(Gravity.START);
+        }
+    }
+
+    /** Populates the navigation drawer with the appropriate items. */
+    private void populateNavDrawer() {
+
+    }
+
+    protected void autoShowOrHideActionBar(boolean show) {
+        if (show == mActionBarShown) {
+            return;
+        }
+
+        mActionBarShown = show;
+        onActionBarAutoShowOrHide(show);
+    }
+
+    protected void onActionBarAutoShowOrHide(boolean shown) {
+        if (mStatusBarColorAnimator != null) {
+            mStatusBarColorAnimator.cancel();
+        }
+        mStatusBarColorAnimator = ObjectAnimator.ofInt(
+                (mDrawerLayout != null) ? mDrawerLayout : mLUtils,
+                (mDrawerLayout != null) ? "statusBarBackgroundColor" : "statusBarColor",
+                shown ? Color.BLACK : mNormalStatusBarColor,
+                shown ? mNormalStatusBarColor : Color.BLACK)
+                .setDuration(250);
+        if (mDrawerLayout != null) {
+            mStatusBarColorAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                    ViewCompat.postInvalidateOnAnimation(mDrawerLayout);
+                }
+            });
+        }
+        mStatusBarColorAnimator.setEvaluator(ARGB_EVALUATOR);
+        mStatusBarColorAnimator.start();
+
+        updateSwipeRefreshProgressBarTop();
+
+        for (View view : mHideableHeaderViews) {
+            if (shown) {
+                view.animate()
+                        .translationY(0)
+                        .alpha(1)
+                        .setDuration(HEADER_HIDE_ANIM_DURATION)
+                        .setInterpolator(new DecelerateInterpolator());
+            } else {
+                view.animate()
+                        .translationY(-view.getBottom())
+                        .alpha(0)
+                        .setDuration(HEADER_HIDE_ANIM_DURATION)
+                        .setInterpolator(new DecelerateInterpolator());
+            }
+        }
+    }
+
+    private void updateSwipeRefreshProgressBarTop() {
+        if (mSwipeRefreshLayout == null) {
+            return;
+        }
+
+        int progressBarStartMargin = getResources().getDimensionPixelSize(
+                R.dimen.swipe_refresh_progress_bar_start_margin);
+        int progressBarEndMargin = getResources().getDimensionPixelSize(
+                R.dimen.swipe_refresh_progress_bar_end_margin);
+        int top = mActionBarShown ? mProgressBarTopWhenActionBarShown : 0;
+        mSwipeRefreshLayout.setProgressViewOffset(false,
+                top + progressBarStartMargin, top + progressBarEndMargin);
+    }
+
+    protected void registerHideableHeaderView(View hideableHeaderView) {
+        if (!mHideableHeaderViews.contains(hideableHeaderView)) {
+            mHideableHeaderViews.add(hideableHeaderView);
+        }
+    }
+
+    protected void deregisterHideableHeaderView(View hideableHeaderView) {
+        if (mHideableHeaderViews.contains(hideableHeaderView)) {
+            mHideableHeaderViews.remove(hideableHeaderView);
+        }
+    }
+
+    protected void setProgressBarTopWhenActionBarShown(int progressBarTopWhenActionBarShown) {
+        mProgressBarTopWhenActionBarShown = progressBarTopWhenActionBarShown;
+        updateSwipeRefreshProgressBarTop();
     }
 }
--- app/src/main/java/com/vashisthg/androidl/MainActivity.java
+++ app/src/main/java/com/vashisthg/androidl/MainActivity.java
@@ -5,15 +5,14 @@ import android.os.Bundle;
 
 public class MainActivity extends BaseActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-
-
-
-
+    @Override
+    protected int getSelfNavDrawerItem() {
+        return NAVDRAWER_ITEM_NOT_INVALID;
+    }
 }
