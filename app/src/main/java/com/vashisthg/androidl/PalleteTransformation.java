636a051221ef4aa9cd82c03923253b58bea02081
Gaurav Vashisth <vashisthg@gmail.com>
2014-11-04 18:58:01 +0530

    getting color from palette

---
+++ app/src/main/java/com/vashisthg/androidl/PalleteTransformation.java
@@ -0,0 +1,39 @@
+package com.vashisthg.androidl;
+
+import android.graphics.Bitmap;
+import android.support.v7.graphics.Palette;
+
+import com.squareup.picasso.Transformation;
+
+import java.util.Map;
+import java.util.WeakHashMap;
+
+/**
+ * Created by vashisthg on 04/11/14.
+ */
+public class PalleteTransformation implements Transformation {
+
+    private static final PalleteTransformation INSTANCE = new PalleteTransformation();
+    private static final Map<Bitmap, Palette> CACHE = new WeakHashMap<Bitmap, Palette>();
+
+    public static PalleteTransformation instance() {
+        return INSTANCE;
+    }
+
+    public static Palette getPalette(Bitmap bitmap) {
+        return CACHE.get(bitmap);
+    }
+
+    private PalleteTransformation() {}
+
+    @Override public Bitmap transform(Bitmap source) {
+        Palette palette = Palette.generate(source);
+        CACHE.put(source, palette);
+        return source;
+    }
+
+    @Override
+    public String key() {
+        return ""; // Stable key for all requests. An unfortunate requirement.
+    }
+}
