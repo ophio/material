4fd6088b25b736c1ee6c6dfc69d610ca3bb172ed
ragdroid <garima.my.way@gmail.com>
2014-11-05 15:52:24 +0530

    circular reveal demo

--- app/src/main/java/com/vashisthg/androidl/view/RevealDemoActivity.java
+++ app/src/main/java/com/vashisthg/androidl/view/RevealDemoActivity.java
@@ -2,8 +2,11 @@ package com.vashisthg.androidl.view;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.annotation.TargetApi;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.widget.ImageView;
@@ -28,18 +31,30 @@ public class RevealDemoActivity extends BaseActivity implements View.OnClickList
     }
 
     @TargetApi(21)
-    private void showFabCircularRevealView(ImageView imageView){
+    private void showFabCircularRevealView(final ImageView imageView){
         if(mLUtils.hasL()) {
             // get the center for the clipping circle
             int cx = (imageView.getLeft() + imageView.getRight()) / 2;
             int cy = (imageView.getTop() + imageView.getBottom()) / 2;
 
+            Log.d("Reveal", String.format(" ImageView Dimens %d %d %d %d", imageView.getTop(), imageView.getLeft(), imageView.getBottom(), imageView.getRight()));
+            Log.d("Reveal", String.format("cx : %d, cy : %d",cx,cy));
             // get the final radius for the clipping circle
             int finalRadius = imageView.getWidth();
             // create and start the animator for this view
             // (the start radius is zero)
             Animator anim =
                     ViewAnimationUtils.createCircularReveal(imageView, cx, cy, 0, finalRadius);
+            anim.setDuration(3000);
+            anim.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+                    super.onAnimationStart(animation);
+                    imageView.setVisibility(View.VISIBLE);
+                }
+            });
+//            ObjectAnimator alphaAnimator = ObjectAnimator.ofFloat(imageView, View.ALPHA, 0, 1);
+//            AnimatorSet
             anim.start();
         }
     }
@@ -69,12 +84,12 @@ public class RevealDemoActivity extends BaseActivity implements View.OnClickList
             int cy = (view.getTop() + view.getBottom()) / 2;
 
             // get the initial radius for the clipping circle
-            int initialRadius = view.getWidth();
+            int initialRadius = view.getWidth()/2;
 
             // create the animation (the final radius is zero)
             Animator anim =
                     ViewAnimationUtils.createCircularReveal(view, cx, cy, initialRadius, 0);
-
+            anim.setDuration(3000);
             // make the view invisible when the animation is done
             anim.addListener(new AnimatorListenerAdapter() {
                 @Override
